837c73189c57f9c18dca0b4cfdae7826f382feee
iGabb <gb0104@hotmail.com>
2022-05-10 12:08:07 -0300

    Update Notas.js

--- Notas.js
+++ Notas.js
@@ -3,4 +3,17 @@ JavaScript básica
 
 // Tudo que vem após barras normais duplas é um comentário em linguagem natural
 
+Var x;                                              // Declara uma variável
+
+// JavaScript aceita vários tipos de valores
+x = 1;                                              // Números.
+x = 0.01;                                           // Apenas um tipo Number para inteiros e reais.
+x = "hello world";                                  // Strings de texto entre aspas.
+x = 'JavaScript';                                   // Apóstrofos também delimitam strings.
+x = true;                                           // Valores booleanos.
+x = false;                                          // O outro valor booleano.
+x = null;                                           // Null é um valor especial que significa "nenhum valor".
+x = undefined;                                      // undefined é como null.
 */
+
+[i]
