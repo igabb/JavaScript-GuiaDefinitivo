ff5e46f444de2976e0b5d147fdddd88539b9211f
iGabb <gb0104@hotmail.com>
2022-05-11 16:29:40 -0300

    Update Notas.js

--- Notas.js
+++ Notas.js
@@ -22,4 +22,4 @@ Um objeto é uma coleção de pares nome/valor ou uma string para mapa de valore
 var book = {                                        // Objetos são colocados entre chaves.
     topic: "JavaScript",                            // A propriedade "topic" tem o valor "javaScript".
     fat: true                                       // A propriedade "fat" tem o valor true.
-}
+};                                                  // A chave marca o fim do objeto.
