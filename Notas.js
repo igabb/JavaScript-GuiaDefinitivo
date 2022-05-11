fa3bee9817eb77a26b2858e69e756ddb24cbe009
iGabb <gb0104@hotmail.com>
2022-05-11 16:42:07 -0300

    Update Notas.js

--- Notas.js
+++ Notas.js
@@ -34,4 +34,5 @@ book.contents = {};                                 // {} é um objeto vazio sem
 var primes = [2, 3, 5, 7];                          // Um array de 4 valores, delimitados com [ e ].
 primes[0]                                           // => 2: o primeiro elemento (índice 0) do array.
 primes.length                                       // => 4: quantidade de elementos no array.
-primes[primes.length-1]                             // => 7: o último elemento do array.
+primes[primes.length - 1]                           // => 7: o último elemento do array.
+primes[4] = 9                                       // Adiciona um novo elemento por meio de atribuição
