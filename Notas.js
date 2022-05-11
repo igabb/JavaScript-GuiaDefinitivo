444dba7cf762ada3e986910c0877a1f87f29a2ca
iGabb <gb0104@hotmail.com>
2022-05-11 16:38:48 -0300

    Update Notas.js

--- Notas.js
+++ Notas.js
@@ -33,3 +33,4 @@ book.contents = {};                                 // {} é um objeto vazio sem
 // JavaScript também aceita arrays (listas indexadas numericamente) de valores.
 var primes = [2, 3, 5, 7];                          // Um array de 4 valores, delimitados com [ e ].
 primes [0]                                          // => 2: o primeiro elemento (índice 0) do array.
+primes.length                                       // => 4: quantidade de elementos no array.
