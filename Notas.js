5236c3fb55a46bae5430f1b62bcb4510ef5ac446
iGabb <gb0104@hotmail.com>
2022-05-14 12:27:30 -0300

    Update Notas.js

--- Notas.js
+++ Notas.js
@@ -65,3 +65,4 @@ count++;                                            // Incrementa a variável
 count--;                                            // Decrementa a variável
 count += 2;                                         // Soma 2: o mesmo que count = count + 2
 count *= 3;                                         // Multiplica por 3: o mesmo que count = count * 3;
+count                                               // => 6: nome de variáveis também são expressões
