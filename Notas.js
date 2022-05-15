d8b12f5de21edda0a984e594e2acded97b574cef
iGabb <gb0104@hotmail.com>
2022-05-15 11:34:35 -0300

    Update Notas.js

--- Notas.js
+++ Notas.js
@@ -42,12 +42,12 @@ empty.length                                        // => 0
 
 // Os arrays e objetos podem conter outros arrays e objetos:
 var points = [                                      // Um array com 2 elementos.
-    { x: 0, y: 0 },                                     // Cada elemento é um objeto.
+    { x: 0, y: 0 },                                 // Cada elemento é um objeto.
     { x: 1, y: 1 }
 ];
 var data = {                                        // Um objeto com 2 propriedades
-    trial1: [[1, 2], [3, 4]],                         // O valor de cada propriedade é um array.
-    trial2: [[2, 3], [4, 5]],                         // O elemento dos arrays são arrays.
+    trial1: [[1, 2], [3, 4]],                       // O valor de cada propriedade é um array.
+    trial2: [[2, 3], [4, 5]],                       // O elemento dos arrays são arrays.
 };
 
 // Os operadores atuam sobre os valores (operando) para produzir um novo valor.
