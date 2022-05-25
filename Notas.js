a18a87674a2b3527bdc1d00dabb012d13343059c
iGabb <gb0104@hotmail.com>
2022-05-25 13:10:55 -0300

    Update Notas.js

--- Notas.js
+++ Notas.js
@@ -157,9 +157,12 @@ var p = new Point(1, 1);                            // O ponto geométrico (1, 1
 // Define métodos para objetos Point atribuindo-os ao objeto
 // prototype associado à função construtora.
 
-Point.prototype.r = function() {
+Point.prototype.r = function () {
     return Math.sqrt(                                   // Retorna a raiz quadrada de x² + y ²
-          this.x * this.x +                               // Este é o objeto Point no qual o método...
+        this.x * this.x +                               // Este é o objeto Point no qual o método...
         this.y * this.y                                 // ... é chamado
-);
+    );
 };
+
+// Agora o objeto Point b (e todos os futuros objetos Point) herda o método r()
+p.r()                                                   // => 1,414...
