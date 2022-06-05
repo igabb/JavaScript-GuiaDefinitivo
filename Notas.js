432d27a167407d74213d348f85acce2bb003677a
iGabb <gb0104@hotmail.com>
2022-06-05 11:34:15 -0300

    Update Notas.js

--- Notas.js
+++ Notas.js
@@ -176,5 +176,5 @@ break       delete      function        return      typeof
 case        do          if              switch      var
 catch       else        in              this        void
 continue    false       instanceof      throw       while
-debugger    finally     
+debugger    finally     new             
 */
