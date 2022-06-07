f92aee7ad86eb3d530359a7207c1c9651cd1b859
iGabb <gb0104@hotmail.com>
2022-06-07 16:40:53 -0300

    Update Notas.js

--- Notas.js
+++ Notas.js
@@ -183,5 +183,5 @@ default     for         null            try
 /*
 JavaScript também reserva certas palavras-chave não utilizada atualmente na linguagem, mas que poderão ser usadas em futuras versões. A ECMAScript 5 reserva as seguintes palavras:
 
-
+class       
 */
