f8fc5ce3606c6d03b31540eb49b410bfe69cf14b
iGabb <gb0104@hotmail.com>
2022-06-08 14:45:42 -0300

    Update Notas.js

--- Notas.js
+++ Notas.js
@@ -190,5 +190,5 @@ Além disso, as seguintes palavras, que são válidas em código JavaScript norm
 implements      let         private     public      yield
 interface       package     protected   static
 
-
+O modo restrito também impões restrições sobre o uso dos identificadores a seguir. eles não são totalmente reservados, mas não são permitidos como nomes de variável, função ou parâmetro:
 */
