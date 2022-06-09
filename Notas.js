ed42094f214aad94318d4d0d6c05f530bd5e5ee5
iGabb <gb0104@hotmail.com>
2022-06-09 18:07:59 -0300

    Update Notas.js

--- Notas.js
+++ Notas.js
@@ -199,5 +199,5 @@ ECMAScript 3 reservou todas as palavras-chave da linguagem Java e, embora tenham
 abstract        double      goto        native      static
 boolean         enum        implements  package     super
 byte            export      import      private     synchronized
-char            
+char            extends
 */
